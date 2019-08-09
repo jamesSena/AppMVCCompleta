7087617ad7634bf9ee3df86e71f8686bb62fd9fd
Jamesson Sales de Sena <jsdesena@interaxa.com>
2019-08-09 15:37:18 -0300

    Validações no ViewModel e não no Model

--- src/DevIO.App/ViewModels/FornecedorViewModel.cs
+++ src/DevIO.App/ViewModels/FornecedorViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,20 @@ namespace DevIO.App.ViewModels
 {
     public class FornecedorViewModel
     {
+        [Key]
+        public Guid ID { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 3)]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(14, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 3)]
+        public string Documento { get; set; }
+        public TipoFornecedor TipoFornecedor { get; set; }
+        public Endereco Endereco { get; set; }
+        [DisplayName("Ativo?")]
+        public bool Ativo { get; set; }
+        public IEnumerable<Produto> Produtos { get; set; }
+
     }
 }
