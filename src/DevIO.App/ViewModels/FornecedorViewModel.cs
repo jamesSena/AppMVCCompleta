0d60c7d447904cae1ffebcf4d8f5a35e65bece80
Jamesson Sales de Sena <jsdesena@interaxa.com>
2019-08-16 14:08:19 -0300

    Melhorando as views dos fornecedores

--- src/DevIO.App/ViewModels/FornecedorViewModel.cs
+++ src/DevIO.App/ViewModels/FornecedorViewModel.cs
@@ -19,7 +19,7 @@ namespace DevIO.App.ViewModels
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(14, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 3)]
         public string Documento { get; set; }
-        public TipoFornecedor TipoFornecedor { get; set; }
+        public int TipoFornecedor { get; set; }
         public Endereco Endereco { get; set; }
         [DisplayName("Ativo?")]
         public bool Ativo { get; set; }
